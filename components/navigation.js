58a00b9e27a53458f52d5837517181f8ba425496
Alex <alex.phan2905@gmail.com>
2021-11-04 23:33:08 +0930

    created styles for background, header, and navigation

--- components/navigation.js
+++ components/navigation.js
@@ -7,13 +7,44 @@ import ProjectsScreen from '../screens/ProjectsScreen';
 import ToolsScreen from '../screens/ToolsScreen';
 
 export default function Navigation() {
+
   const Tab = createBottomTabNavigator();
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Timer" component={TimerScreen} />
-        <Tab.Screen name="Projects" component={ProjectsScreen} />
-        <Tab.Screen name="Tools" component={ToolsScreen} />
+    <NavigationContainer >
+      <Tab.Navigator screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveBackgroundColor: '#921717',
+        tabBarStyle: {
+          backgroundColor: 'brown',
+        },
+        tabBarHideOnKeyboard: true,
+      }}>
+        <Tab.Screen name="Pomodoro Timer" component={TimerScreen} options={{
+          headerStyle: {
+            backgroundColor: 'brown',
+          },
+          headerTitleStyle: {
+            color: 'white',
+          }
+        }} />
+        <Tab.Screen name="Project Manager" component={ProjectsScreen} options={{
+          headerStyle: {
+            backgroundColor: 'brown',
+          },
+          headerTitleStyle: {
+            color: 'white',
+          }
+        }} />
+        <Tab.Screen name="Learning Tools" component={ToolsScreen} options={{
+          headerStyle: {
+            backgroundColor: 'brown',
+          },
+          headerTitleStyle: {
+            color: 'white',
+          }
+        }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
