84396e68d296a317cbbb5117a75febd8ccb24c27
Alex <alex.phan2905@gmail.com>
2021-11-05 08:06:14 +0930

    created add task function

--- components/TasksScreen/TaskList.js
+++ components/TasksScreen/TaskList.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { StyleSheet, KeyboardAvoidingView, ScrollView, TextInput, Text, View, TouchableOpacity, Keyboard } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, KeyboardAvoidingView, ScrollView, TextInput, View, Platform } from 'react-native';
 
 import Heading from '../Heading';
 import Task from './Task';
 
 export default function TaskList() {
+  const [task, addTask] = useState();
+  const [items, addItems] = useState([]);
+
+  const handleAddTask = () => {
+    addItems([...items, task]); // adds items to an array
+    addTask(null); // empties input area
+  }
+
   return (
     <View>
       <ScrollView contentContainerStyle={{ // for scrollable list
@@ -14,14 +22,25 @@ export default function TaskList() {
           <Heading text="To-Do List" />
         </View>
         <View style={styles.list}>
-          <Task text={"Presentation"}/>
-          <Task text={"Simulation Project"}/>
+          {
+            items.map((item, index) => {
+              return <Task key={index} text={item} />
+            })
+          }
         </View>
+        <KeyboardAvoidingView>
+          <View style={styles.container}>
+            <TextInput
+              style={styles.add}
+              placeholder={"+ Add a Task"}
+              value={task}
+              onChangeText={text => addTask(text)}
+              onEndEditing={() => handleAddTask()}
+            />
+          </View>
+        </KeyboardAvoidingView>
       </ScrollView>
-
-      <View style={styles.wrapper}>
-        <TextInput style={styles.add} placeholder={"+ Add a Task"} onChangeText={text => addTask(text)} />
-      </View>
+      
     </View>
   );
 }
@@ -33,11 +52,11 @@ const styles = StyleSheet.create({
   list: {
     alignItems: 'center',
   },
-  wrapper: {
+  container: {
     alignItems: 'center',
   },
   add: {
-    position: 'relative',
+    marginBottom: 10,
     width: '95%',
     padding: 15,
     backgroundColor: 'white',
